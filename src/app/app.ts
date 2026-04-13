454e2a51fffb730dd6c3592222f09180faaa6e4f
EmanuellyBarro31 <barrosemanuelly338@gmail.com>
2026-04-13 16:55:07 -0500

    subindo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,Home],
